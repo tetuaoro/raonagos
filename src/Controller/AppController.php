f5988c080b58e97b9c6054409acc7d531571a1f1
tetuaoro <tetuaoropro@gmail.com>
2021-01-17 12:01:35 -1000

    Upd: set SEO information about this site, delete licence (readme) from public, styling css, say is an site under construction

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="app_")
+ */
 class AppController extends AbstractController
 {
     /**
-     * @Route("/", name="app_")
+     * @Route("/", name="index")
+     * @Route("/accueil", name="home")
      */
     public function index(): Response
     {

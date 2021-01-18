e3b8f37917eb9e6677f4c696cfce6f5c2be89d94
tetuaoro <tetuaoropro@gmail.com>
2021-01-17 20:45:02 -1000

    Upd: add sitemap.xml and robots.txt, optimize SEO website

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @Route("/", name="app_")
@@ -18,5 +19,53 @@ class AppController extends AbstractController
     public function index(): Response
     {
         return $this->render('app/index.html.twig');
+    }
+
+    /**
+     * @Route("/robots.txt", name="robot")
+     */
+    public function robot(RouterInterface $router): Response
+    {
+        $urls = [];
+        $urls[] = $router->generate('app_sitemap', [], false);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/plain');
+
+        return $this->render(
+            'app/robots.txt.twig',
+            [
+                'urls' => $urls,
+            ],
+            $response
+        );
+    }
+
+    /**
+     * @Route("/sitemap.xml", name="sitemap")
+     */
+    public function sitemap(RouterInterface $router): Response
+    {
+        $urls = [];
+
+        $urls[] = [
+            'loc' => $router->generate('app_index', [], false),
+            'priority' => 1,
+        ];
+        $urls[] = [
+            'loc' => $router->generate('app_home', [], false),
+            'priority' => 1,
+        ];
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/xml');
+
+        return $this->render(
+            'app/sitemap.xml.twig',
+            [
+                'urls' => $urls,
+            ],
+            $response
+        );
     }
 }

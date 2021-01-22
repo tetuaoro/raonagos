f3929466afde6d969b5ff6a8135b7b1265f085fb
tetuaoro <tetuaoropro@gmail.com>
2021-01-22 11:09:06 -1000

    Upd : cookies consent, webpack encore to perform script and css, styling page

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+// import './styles/app.css';
+
+import "./styles/base.css";
+import "./styles/vendor.css";
+import "./styles/main.css";
+
+import "./epitome/js/modernizr";
+import "./epitome/js/pace";
+import "./epitome/js/main";
+
+// start the Stimulus application
+// import "./bootstrap";

4bff07db6567897607632068d55a09ec715a5188
tetuaoro <tetuaoropro@gmail.com>
2021-02-07 14:38:37 -1000

    Upd : complete information about rao nagos, style css, optimize rich result

--- webpack.config.js
+++ webpack.config.js
@@ -8,72 +8,24 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 var isProduction = Encore.isProduction();
 
-Encore
-  // directory where compiled assets will be stored
-  .setOutputPath("public/build/")
-  // public path used by the web server to access the output path
+Encore.setOutputPath("public/build/")
   .setPublicPath("/build")
-  // only needed for CDN's or sub-directory deploy
-  //.setManifestKeyPrefix('build/')
-
-  /*
-   * ENTRY CONFIG
-   *
-   * Each entry will result in one JavaScript file (e.g. app.js)
-   * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-   */
   .addEntry("app", "./assets/app.js")
   .addEntry("ch_cookie", "./assets/cookie_consent.js")
   .addStyleEntry("critical", ["./assets/styles/critical.css"])
-
-  // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-  // .enableStimulusBridge('./assets/controllers.json')
-
-  // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
-
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
-  // .enableSingleRuntimeChunk()
   .disableSingleRuntimeChunk()
-
-  /*
-   * FEATURE CONFIG
-   *
-   * Enable & configure other features below. For a full
-   * list of features, see:
-   * https://symfony.com/doc/current/frontend.html#adding-more-features
-   */
   .cleanupOutputBeforeBuild()
   .enableBuildNotifications()
   .enableSourceMaps(!isProduction)
-  // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(isProduction)
-
   .configureBabel((config) => {
     config.plugins.push("@babel/plugin-proposal-class-properties");
   })
-
-  // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
     config.useBuiltIns = "usage";
     config.corejs = 3;
   })
-
-  // enables Sass/SCSS support
-  //.enableSassLoader()
-
-  // uncomment if you use TypeScript
-  //.enableTypeScriptLoader()
-
-  // uncomment if you use React
-  //.enableReactPreset()
-
-  // uncomment to get integrity="..." attributes on your script & link tags
-  // requires WebpackEncoreBundle 1.4 or higher
-  //.enableIntegrityHashes(isProduction)
-
-  // uncomment if you're having problems with a jQuery plugin
   .autoProvidejQuery();
 
 module.exports = Encore.getWebpackConfig();

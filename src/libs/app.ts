982144d8098be7019bf1df38b858106487347777
tetuaoro <tetuaoropro@gmail.com>
2022-11-13 10:47:52 -1000

    fix: meta description

--- src/libs/app.ts
+++ src/libs/app.ts
@@ -4,5 +4,5 @@ export const fbAppId = "100085434258230"
 export const pwaId = "pf.rao-nagos.www"
 export const telephone = "+68987323795"
 export const description =
-  "Rao nagos est une agence numérique qui créée à ta place des sites web personnalisés, des robots de trading uniques et des serveurs de messagerie performants. On est spécialisé dans la création d'un produit final avec une grande expérience utilisateur, un design propre et des fonctionnalités riches."
+  "Rao nagos est une agence numérique qui créée à ta place des sites web personnalisés, des robots automatisés uniques et des serveurs de messagerie performants. On est spécialisé dans la création d'un produit final avec une grande expérience utilisateur, un design propre et des fonctionnalités riches."
 export const sameAs = ["https://www.facebook.com/raonagos", "https://www.twitter.com/raonagos"] // 100085434258230
